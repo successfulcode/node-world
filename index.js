1565f97c94566e97c2fcdd7db643f53a868a187c
successfulcode <56683358+successfulcode@users.noreply.github.com>
2021-09-17 20:57:07 +0300

    Routes

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 require('dotenv').config();
+const path = require('path');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.listen(port, () => {
+  console.log(`Server listening on ${port} port`);
+});
+
 app.get('/', (req, res) => {
   res.send('Hi Node!');
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on ${port} port`);
+app.get('/posts', (req, res) => {
+  res.send('Hi Posts!');
 });
+
+app.get('/users/:id', (req, res) => {
+  res.status(200).send('ok');
+  res.send(`Hi Users!${req.originalUrl} ${req.method} ${req.params.id}`);
+});
+
+app.use('/pictures', express.static(path.join(__dirname, 'assets')));
